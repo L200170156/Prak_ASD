71f10f4de366afeaf2fe7c6ee6542b6356439a85
Muhibah Fata Tika <47974563+L200170156@users.noreply.github.com>
2019-05-15 22:49:56 +0700

    Add files via upload

---
+++ Modulke8_D_156/1.py
@@ -0,0 +1,34 @@
+class Stack(object):
+    def __init__ (self):
+        self.items = []
+    def isEmpty(self):
+        return len(self)==0
+    def __len__(self):
+        return len(self.items)
+    def peek(self):
+        assert not self.isEmpty()
+        return self.items[-1]
+    def pop(self):
+        assert not self.isEmpty()
+        return self.items.pop()
+    def push(self,data):
+        self.items.append(data)
+
+def cetakHexa(d):
+    f = Stack()
+    bil_hexa = ['A','B','C','D','E','F']
+    if d==0: f.push(0);
+    while d!=0:
+        sisa = d%16
+        if (sisa>9):
+            sisa = sisa-10
+            sisa = bil_hexa[sisa]
+           
+        d=d//16
+        f.push(sisa)
+    st = ""
+    for i in range(len(f)):
+        st = st+str(f.pop())
+    return st
+
+

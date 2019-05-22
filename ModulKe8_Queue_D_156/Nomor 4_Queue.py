23bbb3ab850eb6977e938cb7e4f790b6466eeaaf
Muhibah Fata Tika <47974563+L200170156@users.noreply.github.com>
2019-05-23 05:40:53 +0700

    Add files via upload

---
+++ ModulKe8_Queue_D_156/Nomor 4_Queue.py
@@ -0,0 +1,30 @@
+class Queue(object):
+    def __init__(self):
+        self.qlist=[]
+
+    def isEmpty(self):
+        return len(self) ==0
+    def __len__(self):
+        return len(self.qlist)
+    def enqueue(self,data):
+        self.qlist.append(data)
+    def dequeue(self):
+        assert not self.isEmpty()
+        return self.qlist.pop(0)
+    def getFrontMost(self):
+        return self.qlist.pop(0)
+    def getRearMost(self):
+        return self.qlist.pop(-1)
+
+A = Queue()
+A.enqueue("Jeruk")
+A.enqueue("Tomat")
+A.enqueue("Mangga")
+A.enqueue("Duku")
+A.enqueue("Pepaya")
+A.enqueue("Kelengkeng")
+
+front = A.getFrontMost()
+rear = A.getRearMost()
+print ("Nilai maksimal = ", front)
+print ("Nilai minimal = ", rear)
